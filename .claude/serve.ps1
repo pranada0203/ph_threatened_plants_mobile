b49f76ce3141a97408957f121ee150f1ec2e50fa
pranada0203 <pranada55@gmail.com>
2026-08-16 17:58:05 +0800

    UI optimization

---
+++ .claude/serve.ps1
@@ -0,0 +1,29 @@
+$root = "C:\Users\prana\OneDrive\Desktop\ph_threatened_plants_mobile"
+$listener = New-Object System.Net.HttpListener
+$listener.Prefixes.Add("http://localhost:8642/")
+$listener.Start()
+Write-Host "Serving $root on http://localhost:8642/"
+$mime = @{
+  ".html"="text/html"; ".css"="text/css"; ".js"="application/javascript";
+  ".png"="image/png"; ".jpg"="image/jpeg"; ".svg"="image/svg+xml"; ".json"="application/json"
+}
+while ($listener.IsListening) {
+  $context = $listener.GetContext()
+  $req = $context.Request
+  $res = $context.Response
+  $path = $req.Url.LocalPath
+  if ($path -eq "/") { $path = "/index.html" }
+  $filePath = Join-Path $root ($path.TrimStart("/"))
+  if (Test-Path $filePath -PathType Leaf) {
+    $ext = [System.IO.Path]::GetExtension($filePath)
+    $ct = $mime[$ext]
+    if (-not $ct) { $ct = "application/octet-stream" }
+    $bytes = [System.IO.File]::ReadAllBytes($filePath)
+    $res.ContentType = $ct
+    $res.ContentLength64 = $bytes.Length
+    $res.OutputStream.Write($bytes, 0, $bytes.Length)
+  } else {
+    $res.StatusCode = 404
+  }
+  $res.OutputStream.Close()
+}
